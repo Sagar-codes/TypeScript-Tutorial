fab1e879d807ac761bdd50bf1c480c071a11fbac
Sagar-codes <paulsagar5433@gmail.com>
2023-05-21 21:24:39 +0530

    Just Started the Framework

---
+++ dataTypes.js
@@ -0,0 +1,8 @@
+"use strict";
+let a;
+a = "Sagar";
+a.toUpperCase();
+console.log(a);
+let b;
+b = 10;
+console.log(b);
---
+++ dataTypes.ts
@@ -0,0 +1,15 @@
+let a : string;
+
+a = "Sagar";
+
+a.toUpperCase();
+
+console.log(a);
+
+
+let b : number;
+
+b = 10;
+
+console.log(b);
+
